d3681aabfc2873ef4651ba22811ee915c749d965
Hans Mündelein <hans@muendelein.me>
2023-02-16 17:22:54 +0100

    Adjust for new abis structure
    The repo with the abis changed its folder structure. So we needed to
    adjust the build script.

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ use ethers::contract::Abigen;
 use eyre::Report;
 
 fn main() -> Result<(), Report> {
-    Abigen::new("ColonyNetwork", "./abis/abis/glwss2/ColonyNetwork.json")?
+    Abigen::new("ColonyNetwork", "./abis/dist/versions/glwss2/ColonyNetwork.json")?
         .generate()?
         .write_to_file("./src/contracts/colony_network.rs")?;
-    Abigen::new("Colony", "./abis/abis/glwss2/IColony.json")?
+    Abigen::new("Colony", "./abis/dist/versions/glwss2/IColony.json")?
         .generate()?
         .write_to_file("./src/contracts/colony.rs")?;
     Ok(())
